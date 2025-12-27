dbd77e2b0b14c17e6badaeea502713e1451e74ff
techyguyperplexable <objecting@objecting.org>
2025-12-28 00:05:00 +0530

    sched: reduce migration cost for faster load balancing
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -95,7 +95,7 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
 unsigned int sysctl_sched_wakeup_granularity		= 500000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity	= 500000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 250000UL;
 
 #ifdef CONFIG_SMP
 /*
