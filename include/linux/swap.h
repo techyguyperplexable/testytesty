855681d10eb38fd3a68a3397016ba0e40e278908
techyguyperplexable <objecting@objecting.org>
2025-12-28 00:08:46 +0530

    mm: increase SWAP_CLUSTER_MAX for better batching
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -176,7 +176,7 @@ enum {
 	SWP_SCANNING	= (1 << 12),	/* refcount in scan_swap_map */
 };
 
-#define SWAP_CLUSTER_MAX 32UL
+#define SWAP_CLUSTER_MAX 64UL
 #define COMPACT_CLUSTER_MAX SWAP_CLUSTER_MAX
 
 #define SWAP_MAP_MAX	0x3e	/* Max duplication count, in first swap_map */
