cf7b7433188b2f886e93166792faa7298128f957
techyguyperplexable <objecting@objecting.org>
2025-12-27 23:59:17 +0530

    gpu: improve interactive governor responsiveness
    Signed-off-by: techyguyperplexable <objecting@objecting.org>

--- drivers/gpu/arm/v_r17p0/platform/exynos/mali_kbase_platform.c
+++ drivers/gpu/arm/v_r17p0/platform/exynos/mali_kbase_platform.c
@@ -260,8 +260,8 @@ static int gpu_dvfs_update_config_data_from_dt(struct kbase_device *kbdev)
 	if (!strncmp("interactive", of_string, strlen("interactive"))) {
 		platform->governor_type = G3D_DVFS_GOVERNOR_INTERACTIVE;
 		gpu_update_config_data_int_array(np, "interactive_info", of_data_int_array, 3);
-		platform->interactive.highspeed_clock = of_data_int_array[0] == 0 ? 500 : (u32) of_data_int_array[0];
-		platform->interactive.highspeed_load  = of_data_int_array[1] == 0 ? 100 : (u32) of_data_int_array[1];
+		platform->interactive.highspeed_clock = of_data_int_array[0] == 0 ? 702 : (u32) of_data_int_array[0];
+		platform->interactive.highspeed_load  = of_data_int_array[1] == 0 ? 60 : (u32) of_data_int_array[1];
 		platform->interactive.highspeed_delay = of_data_int_array[2] == 0 ? 0 : (u32) of_data_int_array[2];
 	} else if (!strncmp("static", of_string, strlen("static"))) {
 		platform->governor_type = G3D_DVFS_GOVERNOR_STATIC;
